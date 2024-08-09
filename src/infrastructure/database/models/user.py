8f034f299c05d3de67bc8349eb7e77f56e6149d5
majaja <majajashka@hotmail.com>
2024-08-09 20:55:21 +0400

    feat(ORM models): add User model

---
+++ src/infrastructure/database/models/user.py
@@ -0,0 +1,19 @@
+from sqlalchemy import BigInteger, LargeBinary, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.infrastructure.database.models.base import Base, TimestampMixin
+from src.core.models.dto import UserDTO
+
+
+class User(Base, TimestampMixin):
+    __tablename__ = 'users'
+
+    id = Mapped[int] = mapped_column(BigInteger, autoincrement=True, primary_key=True)
+    user_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False)
+    login: Mapped[str] = mapped_column(String(30), nullable=False)
+    email: Mapped[str] = mapped_column(String(255), unique=True)
+    password_hash: Mapped[bytes] = mapped_column(LargeBinary, nullable=False)
+    password_salt: Mapped[bytes] = mapped_column(LargeBinary, nullable=False)
+
+    def to_dto(self) -> UserDTO:
+        return UserDTO.from_orm(self)
